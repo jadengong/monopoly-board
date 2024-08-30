fbc2958d2274a4240483585f1b6bd7d3db87b9ce
Jaden Ong <ong.jadengabriel@gmail.com>
2024-08-29 19:57:12 -0700

    first draft (pending compilation issues)
    checkwinner: created a 2d array (or list of lists if you will) of winning positions and used a function signature to declare a generic type of winning position and player position to be used in a containsAll. draws included
    
    determinePlayer: very tacky, just random num from 1 to 100 and if its greater than half its one player's turn to start and its the other if not.
    
    main function: currentPosition and currentPlayer variables used from default initialized var in constructor. updated valid position checker when entering a position. checkwinner implemented immediately after a valid input. swap player feature at the end of the loop.

--- Ong_Jaden.cpp
+++ Ong_Jaden.cpp
@@ -1,4 +1,5 @@
 
+#include <algorithm>
 #include <iostream>
 #include <vector>
 #include <list>
@@ -10,18 +11,20 @@ public:
     TicTacToe(){
         /* Initialize defaults here */
 
-        char currentPlayer = ''; // either X or O
+        char currentPlayer = ' '; // to be set to x or 0
         int currentPosition = 0;
 
 
     }
 
+    // lists for player entries
+    std::vector<int> playerXPositions;
+    std::vector<int> playerOPositions;
+
 
 
 
     /* This is your game board*/
-
-
     vector<vector<char>> gameBoard
 {
     {'1', '2', '3'},
@@ -46,34 +49,62 @@ public:
 
     int main() {
 
+            printGameBoard();
+            char currentPlayer = determinePlayer();
 
         while(true) {
 
-            int playerTemp;
+
+            cout << "It is player " << currentPlayer << "'s turn.";
+            cout << "Player " << currentPlayer << ", please enter an open position on the gameboard.";
 
 
-            cout << "It is player " << playerTemp << "'s turn.";
-            cout << "Player " << playerTemp << ", please enter an open position on the gameboard.";
-
-            int positionTemp;
-            cin >> positionTemp;
-            while(positionTemp < 1 || positionTemp > 9 || positionTemp != ' '){
-                cout << "Invalid input, please enter another position." << endl;
+            // input
+            int currentPosition;
+            cin >> currentPosition;
+            while(currentPosition < 1 || currentPosition > 9){
+                if(playerXPositions[currentPosition] != ' ' || playerOPositions[currentPosition] != ' ') {
+                    cout << "Invalid input, please enter another position." << endl;
+                }
             }
 
-                modifyGameBoard(playerTemp, positionTemp);
+                modifyGameBoard(gameBoard, currentPlayer, currentPosition); // playerPosition lists updated there
                 std::string result = checkWinner();
-                if(result.length() > 0) {
+                if(result.length() > 0) { // have to check winner after every entry in the event that the last entry is a winning one
                     cout << result << endl;
                     break;
                 }
 
 
+            // switch players
+            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
 
             }
+        return 0; // i have learned that main has to be int and returns 0 on close.
     }
 
+    char determinePlayer() { // kind of random way to decide who starts
+        int temp;
+        temp = rand() % 100 + 1; // 1 to 100
+        if(temp > 50) {
+            return 'X';
+        }else {
+            return 'O';
+        }
+    }
+
+
+    bool containsAll(const std::vector<int> & playerPositions, const std::vector<int> & winningPositions) { // i guess this is what you call a function signature that sets a generic template to be used later
+        // c++ lists have .begin and .end when iterating; as well as an all_of which acts like the containsAll() in java; and then there is a find function that helps me compare an element to target
+        return std::all_of(winningPositions.begin(), winningPositions.end(), [&](int pos) { // last condition sets a lambda by creating a very temporary variable pos to be used as a comparison in the                                                                                                        next line
+            return std::find(playerPositions.begin(), playerPositions.end(), pos) != playerPositions.end(); // iterate through the entire player position (to be either X or O) and compare (that is what the                                                                                                              last condition is, checking if the search for pos was successful or not)
+        });
+
+    }
+
+
     std::string checkWinner() {
+        // use a list of lists to contain possible winning positions and then check those conditions
 
         std::vector<std::vector<int>> winningPositions = {
             {1,2,3}, {4,5,6}, {7,8,9}, // row wins
@@ -82,9 +113,17 @@ public:
 
         };
 
-        for() {
+        for(auto a : winningPositions) {
+            if(containsAll(playerOPositions, a)) {
+                return "Player O wins!";
+            }else if(containsAll(playerXPositions, a)) {
+                return "Player X wins!";
+            }else if(playerOPositions.size() + playerXPositions.size() == 9) {
+                return "The game ends in a draw.";
+            }
+        }
 
-
+        return "";
 
     }
 
@@ -93,14 +132,50 @@ public:
 
 
     /* This method modifes the game board*/
-    void modifyGameBoard(char player, int position) // place piece
+    void modifyGameBoard(vector<vector<char>> gameBoard, char player, int position) // added the gameboard parameter, but i realized i can't just pass char[][], so i had to google what the c++ equivalent was
 
     {
         /*write code to modify the game board here*/
 
+        player = ' ';
 
-        gameBoard[][] =
+        if(player == 'X') {
+            playerXPositions.push_back(position);
+        }else {
+            playerOPositions.push_back(position);
+        }
 
+        switch(position) {
+            case 1:
+                gameBoard[0][0] = player;
+                break;
+            case 2:
+                gameBoard[0][2] = player;
+                break;
+            case 3:
+                gameBoard[0][4] = player;
+                break;
+            case 4:
+                gameBoard[2][0] = player;
+                break;
+            case 5:
+                gameBoard[2][2] = player;
+                break;
+            case 6:
+                gameBoard[2][4] = player;
+                break;
+            case 7:
+                gameBoard[4][0] = player;
+                break;
+            case 8:
+                gameBoard[4][2] = player;
+                break;
+            case 9:
+                gameBoard[4][4] = player;
+                break;
+            default:
+                break;
+        }
 
 
 
