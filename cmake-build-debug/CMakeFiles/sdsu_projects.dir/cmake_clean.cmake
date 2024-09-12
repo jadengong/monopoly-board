bfcf7764168bb49fff5fcfe6c0fb47b9cbcb830a
Jaden Ong <ong.jadengabriel@gmail.com>
2024-09-11 17:45:33 -0700

    test

---
+++ cmake-build-debug/CMakeFiles/sdsu_projects.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sdsu_projects.dir/Ong_Jaden.cpp.o"
+  "CMakeFiles/sdsu_projects.dir/Ong_Jaden.cpp.o.d"
+  "sdsu_projects"
+  "sdsu_projects.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sdsu_projects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/sdsu_projects.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for sdsu_projects.
+# This may be replaced when dependencies are built.
