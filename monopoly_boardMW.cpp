e93cc70d88f4fd96c1e4c80a911fbd65095426b7
Jaden Ong <ong.jadengabriel@gmail.com>
2024-09-24 20:23:57 -0700

    program 1 progress
    deleteAtHead, AtTail
    insertAtTail,
    
    WIP delete/insert at position

--- monopoly_boardMW.cpp
+++ monopoly_boardMW.cpp
@@ -71,26 +71,105 @@ public:
         }
 
       cout<<"Insert at head unwritten"<<endl;
+
     }
 
 
-    void insertAtTail() {
+    void insertAtTail(T value) {
+
+        Node<T> *newNode = new Node<T>(value);
+        if(headNode == nullptr) {
+            headNode = newNode; // if empty, just enter
+        } else {
+            Node<T> *temp = headNode; // use a pointer and traverse until next is the end
+            while(temp->nextNode != nullptr) {
+                temp = temp->nextNode;
+            }
+            temp->nextNode = newNode;
+        }
+
         cout<<"Insert at Tail unwritten"<<endl;
     }
 
-    void insertAtPosition() {
+    void insertAtPosition(T value, T position) {
+
+        Node<T> *newNode = new Node<T>(value);
+        if(headNode == nullptr) { // as always, if list is empty, just enter
+            headNode = newNode;
+        } else {
+            Node<T> *temp = headNode; // using a pointer until we reach position desired
+            while(temp->nextNode != position) { // iterate until we reach position wanted
+                temp = temp->nextNode;
+            }
+            // insert newNode with the value at position
+            temp->nextNode = newNode;
+        }
+
+
         cout<<"Insert at Position unwritten"<<endl;
     }
+
+
     void deleteAtHead() {
+
+        if(headNode == nullptr) { // all three delete methods will have a check for an empty list
+            cout << "List is empty, can't delete" << endl;
+            return;
+        }
+
+        // deleting at the head doesn't require a T value input or an if-statement other than if the list is empty:
+
+        Node<T> *toDelete = headNode; // mark first node first
+        headNode = headNode->nextNode; // moving headNode pointer to next
+        delete toDelete;
+
+
         cout<<"Delete at head unwritten"<<endl;
     }
 
 
     void deleteAtTail() {
+
+        if(headNode == nullptr) { // all three delete methods will have a check for an empty list
+            cout << "List is empty, can't delete" << endl;
+            return;
+        }
+
+        // deleting at the tail doesn't require a T value input or an if-statement other than if the list is empty: we can set the marker for delete to be the one previous from the tail
+
+        // must iterate to get there first
+
+        Node<T> *temp = headNode;
+        while(temp->nextNode != nullptr && !temp->nextNode->nextNode == nullptr) { // added check if list hasn't reached end to be safe, could probably remove
+            temp = temp->nextNode;
+        }
+
+        Node<T> *toDelete = temp->nextNode;
+        temp->nextNode = toDelete->nextNode;
+        delete toDelete;
+
         cout<<"Delete at Tail unwritten"<<endl;
     }
 
-    void deleteAtPosition() {
+    void deleteAtPosition(T value, T position) {
+
+        if(headNode == nullptr) { // all three methods will have a check for an empty list
+            cout << "List is empty, can't delete at position: " << position << endl;
+            return;
+        }
+
+        Node<T> *temp = headNode;
+        while(temp->nextNode != nullptr && !temp->nextNode.isEqual(position)) { // iterate if haven't reached the end and if haven't reached our position
+            temp = temp->nextNode;
+        }
+
+        if(temp->nextNode == nullptr) { // the end
+            cout << "Reached end of list, position entered not possible" << endl;
+        } else { // found
+            Node<T> *toDelete = temp->nextNode; // mark for deletion
+            temp->nextNode = temp->nextNode->nextNode; // connect prev to next
+            delete toDelete;
+        }
         cout<<"Delete at Position unwritten"<<endl;
     }
 
@@ -149,30 +228,35 @@ int main() {
     // Insert elements at the end
     list.insertAtHead(MonopolyBoard("Boardwalk", "Dark Blue", 400, 50));
 
-    list.insertAtTail();
-
-    list.insertAtPosition();
+    list.insertAtTail(MonopolyBoard("Mayfair", "Dark Blue", 400, 50));
 
     list.deleteAtHead();
-
     list.deleteAtTail();
 
-    list.deleteAtPosition();
+    // list.insertAtPosition(MonopolyBoard("Kentucky Avenue", "Red", 220, 18), 5);
+
+    // list.deleteAtHead();
+    //
+    // list.deleteAtTail();
+
+    // list.deleteAtPosition();
 
     //Optional Level 1 Tasks
 
-    list.reverseCLList();
-    list.sortCLList();
-    list.printHeadNode();
-    list.printLastNode();
-    list.isListEmpty();
-    list.countNodes();
+    // list.reverseCLList();
+    // list.sortCLList();
+     // list.printHeadNode();
+     // list.printLastNode();
+    // list.isListEmpty();
+    // list.countNodes();
 
-    //Optional Level 2 Tasks
-    list.convertCLList();
-    list.updateNodeValue();
-    list.displaySpecificColorNode();
-    list.mergeCLList();
+    list.printList();
+
+    // //Optional Level 2 Tasks
+    // list.convertCLList();
+    // list.updateNodeValue();
+    // list.displaySpecificColorNode();
+    // list.mergeCLList();
 
     return 0;
 }
